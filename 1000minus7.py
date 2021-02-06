aa3f5aa778aac9f24146776154a3aa26044407b2
quietw <59100155+iamquietw@users.noreply.github.com>
2021-02-06 11:19:59 -0800

    Create 1000minus7.py

---
+++ 1000minus7.py
@@ -0,0 +1,13 @@
+import time
+
+print("я..")
+time.sleep(1)
+print("гуль...")
+time.sleep(1)
+print("ле ле ле ми дай")
+time.sleep(0.5)
+x = 1000
+while x > 5:
+    x = x - 7
+    print(f"{x+7} - 7 = {x}")
+    time.sleep(0.05)
